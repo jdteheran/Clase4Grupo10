c35235a90fcad3453091f77d17d7e05681f63100
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-15 18:32:19 -0500

    algoritmo casi terminado

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hola mundo, grupo de programacion 10');
+
+let numero = 17
+
+function esPrimo(num) {
+    let contDivisores = 0
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0)  {
+            contDivisores = contDivisores +1 
+        }        
+    }
+}
+
