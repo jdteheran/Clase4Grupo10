37f70c324dffc8209c60dc39015d53e2fd75d4de
Juan Teheran <jdteheran@uninorte.edu.co>
2021-11-15 19:09:05 -0500

    commit realizado desde la rama dv2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
 let numero = 32
+let numero2 = 34
+let numero3 = 37
+let numero4 = 39
 
 console.log(esPrimo(numero));
+console.log(esPrimo(numero2));
+console.log(esPrimo(numero3));
+console.log(esPrimo(numero4));
 
 function esPrimo(num) {
     let contDivisores = 0
